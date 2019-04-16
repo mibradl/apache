2c3fc1cda79270df950c6c6a0a312103bd52f2f3
Michael Bradley <mibradl@hotmail.com>
2019-04-16 19:41:47 +0000

    add test data

--- test/integration/default/default_test.rb
+++ test/integration/default/default_test.rb
@@ -5,13 +5,23 @@
 # The Inspec reference, with examples and extensive documentation, can be
 # found at http://inspec.io/docs/reference/resources/
 
-unless os.windows?
-  # This is an example test, replace with your own test.
-  describe user('root'), :skip do
-    it { should exist }
-  end
+describe port(80) do
+  it{should be_listening}
 end
 
+describe package('apache2') do
+  it{should be_installed}
+end
+
+describe fiel('/var/www/html/index.html') do
+  it{should exist}
+  its('content'){should match(/Hello Pipleline World!/}
+end
+
+describe upstart_service('apache2') do
+  it{should be_enabled}
+  it{should be_running}
+end
 # This is an example test, replace it with your own test.
 describe port(80), :skip do
   it { should_not be_listening }
